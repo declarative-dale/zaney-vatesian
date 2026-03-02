39b8117fcf10206f9e39c5e6c9cb547fb74d92e4
Don Williams <don.e.williams@gmail.com>
2026-03-02 04:09:26 -0500

    Updated flake and overlay to fix deno build issue
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   flake.nix
    	modified:   modules/core/overlays.nix

--- flake.nix
+++ flake.nix
@@ -7,6 +7,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/release-25.05";
     nvf.url = "github:notashelf/nvf";
     stylix.url = "github:danth/stylix/release-25.11";
     nix-flatpak.url = "github:gmodena/nix-flatpak?ref=latest";
