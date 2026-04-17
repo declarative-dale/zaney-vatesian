6462b68fdc7ee52c09acfe12904ed8cbb5f81d45
Dale <mail@dalemorgan.us>
2026-04-17 06:05:25 -0500

    xps-v: fix espanso keyboard and input access

--- hosts/xps-v/espanso.nix
+++ hosts/xps-v/espanso.nix
@@ -1,10 +1,15 @@
 {username, ...}: {
+  users.users.${username}.extraGroups = ["input"];
+
   home-manager.users.${username}.services.espanso = {
     enable = true;
 
     # Generates ~/.config/espanso/config/default.yml
     configs.default = {
       show_notifications = false;
+      keyboard_layout = {
+        layout = "us";
+      };
     };
 
     # Generates ~/.config/espanso/match/base.yml
